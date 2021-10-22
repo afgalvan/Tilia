f8144c1b631e6fcae1389a844be564237d2b32ff
Andres <afgalvan@unicesar.edu.co>
2021-10-22 07:49:55 +0100

    fix: add relation for medical file

--- src/Server/Domain/Patients/Patient.cs
+++ src/Server/Domain/Patients/Patient.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Locations;
+using Domain.MedicalFiles;
 using Domain.People;
 
 namespace Domain.Patients
@@ -9,6 +10,7 @@ namespace Domain.Patients
         public string      Studies     { get; set; }
         public SportsData  SportsData  { get; set; }
         public ContactData ContactData { get; set; }
+        public MedicalFile MedicalFile { get; set; }
 
         public Patient(PersonId id, PersonName names, Genre genre, Location location,
             SportsData sportsData, ContactData contactData) : base(id, names, genre, location)
