9000d84be29989131857285b5c9b73c85aace25b
jwar28 <javierguerra2203@gmail.com>
2021-10-23 10:50:36 -0500

    feat: content area util class added

--- src/Client/Presentation/Components/MedAppointment/QueryMedicalAppointmentUserControl.xaml.cs
+++ src/Client/Presentation/Components/MedAppointment/QueryMedicalAppointmentUserControl.xaml.cs
@@ -1,33 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Presentation.Components.MedicalAppointment
+namespace Presentation.Components.MedAppointment
 {
     /// <summary>
-    /// Lógica de interacción para MedicalAppointmentUserControl.xaml
+    /// Lógica de interacción para QueryMedicalAppointmentUserControl.xaml
     /// </summary>
-    public partial class MedicalAppointmentUserControl : UserControl
+    public partial class QueryMedicalAppointmentUserControl : UserControl
     {
-        public MedicalAppointmentUserControl()
+        public QueryMedicalAppointmentUserControl()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
