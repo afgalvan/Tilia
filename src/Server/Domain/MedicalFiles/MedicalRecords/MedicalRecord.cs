5271ed5ea5828428bf539c4a52a957f459ff482f
Andres <afgalvan@unicesar.edu.co>
2021-10-27 15:34:43 +0100

    fix: change multiplicity between records & exams

--- src/Server/Domain/MedicalFiles/MedicalRecords/MedicalRecord.cs
+++ src/Server/Domain/MedicalFiles/MedicalRecords/MedicalRecord.cs
@@ -4,20 +4,17 @@ namespace Domain.MedicalFiles.MedicalRecords
 {
     public class MedicalRecord
     {
-        public Anamnesis Anamnesis { get; set; }
-        public IList<PhysicalExam> PhysicalExams { get; set; }
+        public Anamnesis    Anamnesis     { get; set; }
+        public PhysicalExam PhysicalExams { get; set; }
 
         public MedicalRecord(string description)
         {
             Anamnesis = new Anamnesis(description);
-            PhysicalExams = new List<PhysicalExam>();
         }
 
-        public void AddPhysicalExam(VitalSign vitalSignResults,
-            IList<BodyPartRecord> bodyPartRecords)
+        public void AddPhysicalExam(VitalSign vitalSignResults, IList<BodyPartRecord> bodyPartRecords)
         {
-            var physicalExam = new PhysicalExam(vitalSignResults, bodyPartRecords);
-            PhysicalExams.Add(physicalExam);
+            PhysicalExams = new PhysicalExam(vitalSignResults, bodyPartRecords);
         }
     }
 }
