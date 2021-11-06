a6145d9a63f8e304d56643b012446e6b9654ef0a
Andres <afgalvan@unicesar.edu.co>
2021-11-06 14:29:54 +0100

    refactor: change domain entities relations

--- src/Server/Domain/MedicalFiles/MedicalOrders/MedicalOrder.cs
+++ src/Server/Domain/MedicalFiles/MedicalOrders/MedicalOrder.cs
@@ -1,22 +1,15 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace Domain.MedicalFiles.MedicalOrders
 {
     public class MedicalOrder
     {
         public AptitudeCertificate AptitudeCertificate { get; set; }
-        public IList<Referrals>    Referrals           { get; set; }
         public IList<Inability>    Inabilities         { get; set; }
 
         public MedicalOrder(AptitudeCertificate aptitudeCertificate)
         {
             AptitudeCertificate = aptitudeCertificate;
-        }
-
-        public void AddReferrals(string referralDepartment)
-        {
-            Referrals.Add(new Referrals(referralDepartment));
         }
 
         public void AddInability(string inabilityDescription)
