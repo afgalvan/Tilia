131f4e12266087fd68cddc12c38e753042654e51
Andres <afgalvan@unicesar.edu.co>
2021-11-06 17:45:24 +0100

    fix: solve missing class

---
+++ src/Shared/Domain/MedicalFiles/MedicalNotes/Referral.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.MedicalFiles.MedicalNotes
+{
+    public class Referral
+    {
+        public string Department  { get; set; }
+        public string Description { get; set; }
+
+        public Referral(string department, string description)
+        {
+            Department  = department;
+            Description = description;
+        }
+    }
+}
