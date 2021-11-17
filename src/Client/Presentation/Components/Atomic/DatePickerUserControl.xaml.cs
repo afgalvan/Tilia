6ff976732031c82acde869af7cac3a0980c8c24c
jwar28 <javierguerra2203@gmail.com>
2021-11-16 23:31:30 -0500

    fix: reusable date picker fixed

--- src/Client/Presentation/Components/Atomic/DatePickerUserControl.xaml.cs
+++ src/Client/Presentation/Components/Atomic/DatePickerUserControl.xaml.cs
@@ -11,15 +11,40 @@ namespace Presentation.Components.Atomic
         public DatePickerUserControl()
         {
             InitializeComponent();
+            SetDefaultDatePicker();
         }
 
-        public string Hint
+        public string DatePickerHint
         {
-            get => (string)GetValue(HintProperty);
-            set => SetValue(HintProperty, value);
+            get => (string)GetValue(DatePickerHintProperty);
+            set => SetValue(DatePickerHintProperty, value);
         }
 
-        public static readonly DependencyProperty HintProperty =
-            DependencyProperty.Register("Hint", typeof(string), typeof(DatePickerUserControl), new PropertyMetadata(null));
+        public string DatePickerWidth
+        {
+            get => (string)GetValue(DatePickerWidthProperty);
+            set => SetValue(DatePickerWidthProperty, value);
+        }
+
+        public string DatePickerFontSize
+        {
+            get => (string)GetValue(DatePickerFontSizeProperty);
+            set => SetValue(DatePickerFontSizeProperty, value);
+        }
+
+        private void SetDefaultDatePicker()
+        {
+            DatePickerWidth = "200";
+            DatePickerFontSize = "16";
+        }
+
+        public static readonly DependencyProperty DatePickerHintProperty =
+            DependencyProperty.Register("DatePickerHint", typeof(string), typeof(DatePickerUserControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty DatePickerWidthProperty =
+            DependencyProperty.Register("DatePickerWidth", typeof(string), typeof(DatePickerUserControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty DatePickerFontSizeProperty =
+            DependencyProperty.Register("DatePickerFontSize", typeof(string), typeof(DatePickerUserControl), new PropertyMetadata(null));
     }
 }
